95100a254c70d8b968b20b2c9ddde0989795937c
ezramizrahi <mizrahi.ez@gmail.com>
2021-02-20 19:09:14 +0800

    commands

---
+++ cypress/support/cucumber-html-reporter.js
@@ -0,0 +1,19 @@
+const reporter = require('cucumber-html-reporter');
+
+const options = {
+  theme: 'hierarchy',
+  jsonDir: 'cypress/cucumber-json',
+  output: 'cypress/reports/cucumber_report.html',
+  reportSuiteAsScenarios: true,
+  scenarioTimestamp: true,
+  launchReport: true,
+  ignoreBadJsonFile: true,
+  scenarioTimestamp: true,
+  metadata: {
+    "App Version": "1.0.0",
+    "Test Environment": "DEV",
+    "Browser": "Chrome"
+  }
+};
+
+reporter.generate(options);
